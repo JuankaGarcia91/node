08c22b17bd014a14c78e147b73ad43772b79b9a9
JuankaGarcia91 <juankag.l91@gmail.com>
2023-02-27 12:05:19 -0500

    node

---
+++ ejemplos/fsexample.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+function read(route, callBack){
+    fs.readFile(route, (err, data)=>{
+        //console.log(data);
+        callBack(data.toString())
+    })    
+}
+
+function write(route, content, callBack){
+    fs.writeFile(route, content, (err)=>{
+        if(err)
+            console.error(`Error Writing file: ${err}`)
+        else
+            console.log('Success!!!')
+    })
+}
+//write(`${__dirname}/MyFile.txt`, 'Hello im the content')
+read(`${__dirname}/MyFile.txt`, console.log)
---
+++ ejemplos/variablesentorno.js
@@ -0,0 +1,4 @@
+let saludo = process.env.NOMBRE || 'NN'
+let city = process.env.CITY || 'No where'
+
+console.log(`Welcome ${saludo} you are from ${city}`)
